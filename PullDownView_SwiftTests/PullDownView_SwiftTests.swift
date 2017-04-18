3f32948d0a7f4cdd691c93ad23dbc7fb53fef9d7
yangyangzhang <zyydeveloper@163.com>
2017-04-18 10:29:30 +0800

    Initial Commit

---
+++ PullDownView_SwiftTests/PullDownView_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  PullDownView_SwiftTests.swift
+//  PullDownView_SwiftTests
+//
+//  Created by Tech-zhangyangyang on 2017/4/18.
+//  Copyright © 2017年 Tech-zhangyangyang. All rights reserved.
+//
+
+import XCTest
+@testable import PullDownView_Swift
+
+class PullDownView_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
